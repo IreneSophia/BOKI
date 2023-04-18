e41e4f370ae57ab1f472f1fd2ba5e87559c7c81d
Irene Sophia Plank <10planki@gmail.com>
2023-04-18 12:38:35 +0200

    Finished preprocessing of CentraXX output. However, some participants have missing demographics values - needs to be checked out.

--- preproCentraXX.R
+++ preproCentraXX.R
@@ -96,7 +96,6 @@ df.demo = df %>% filter(questionnaire == "PSY_BOKI_DEMO_Neu") %>%
                   `PSY_PIPS_Demo_Sehhilfe/glasses` = "vision", 
                   `PSY_PIPS_DEMO_SCHREIBHAND` = "handedness",
                   `PSY_BOKI_DEMO_5` = "hobbies"),
-    item = gsub("^PSY_NEVIA_DEMO_.*", "cis", item),
     value = case_when(
       grepl("keine|nein", value, ignore.case = TRUE) ~ "0",
       grepl("ja", value, ignore.case = TRUE) ~ "1",
@@ -158,31 +157,101 @@ df.rap = df %>% filter(questionnaire == "BOKI_Rapport_Fragebogen") %>%
   )
 
 # PSY_BOKI_SPF
-df.spf = df %>% filter(questionnaire == "PSY_BOKI_SPF") 
-
+df.spf = df %>% filter(questionnaire == "PSY_BOKI_SPF") %>%
+  select(SID, numericValue, item) %>%
+  mutate(numericValue = as.numeric(numericValue)) %>%
+  pivot_wider(values_from = numericValue, names_from = item) %>%
+  mutate(
+    spf_e = PSY_PIPS_SPF_Q1 + PSY_PIPS_SPF_Q4 + PSY_PIPS_SPF_Q6  + PSY_PIPS_SPF_Q8,
+    spf_f = PSY_PIPS_SPF_Q2 + PSY_PIPS_SPF_Q5 + PSY_PIPS_SPF_Q9  + PSY_PIPS_SPF_Q11,
+    spf_p = PSY_PIPS_SPF_Q3 + PSY_PIPS_SPF_Q7 + PSY_PIPS_SPF_Q10 + PSY_PIPS_SPF_Q12,
+    spf_total = spf_e + spf_f + spf_p
+  ) %>%
+  select(SID, spf_e, spf_f, spf_p, spf_total)
 
 # PSY_BOKI_BSL-23
-df.bsl = df %>% filter(questionnaire == "PSY_BOKI_BSL-23") 
-
+df.bsl = df %>% filter(questionnaire == "PSY_BOKI_BSL-23") %>%
+  mutate(
+    numericValue = case_when(
+      is.na(numericValue) ~ value,
+      TRUE ~ numericValue
+    ),
+    numericValue = as.numeric(numericValue),
+    section = case_when(
+      item == "PSY_BOKI_BSL-23_Schieber" ~ "state",
+      substr(item,16,16) == "_" ~ "sym", 
+      substr(item,16,16) == "." ~ "beh"
+    )
+  ) %>% select(SID, section, numericValue) %>%
+  group_by(SID, section) %>%
+  summarise(
+    BSL = sum(numericValue, na.rm = T)
+  ) %>%
+  pivot_wider(values_from = BSL, names_from = section, names_prefix = "BSL_")
 
 # PSY_BOKI_TAS
-df.tas = df %>% filter(questionnaire == "PSY_BOKI_TAS") 
+df.tas = df %>% filter(questionnaire == "PSY_BOKI_TAS") %>%
+  mutate(
+    item = as.numeric(gsub("PSY_BOKI_TAS_", "", item)),
+    numericValue = as.numeric(numericValue)
+  ) %>%
+  select(SID, item, numericValue)
 
+# some need to be turned around
+idx = c(4, 5, 10, 18, 19)
+df.tas[df.tas$item %in% idx,]$numericValue = abs(df.tas[df.tas$item %in% idx,]$numericValue - 6)
+
+df.tas = df.tas %>%
+  group_by(SID) %>%
+  summarise(
+    tas_total = sum(numericValue, na.rm = T)
+  )
 
 # PSY_BOKI_SMS
-df.sms = df %>% filter(questionnaire == "PSY_BOKI_SMS") 
+df.sms = df %>% filter(questionnaire == "PSY_BOKI_SMS") %>%
+  mutate(
+    numericValue = case_when(
+      value == "wahr" ~ 1,
+      value == "falsch" ~ 0
+    ), 
+    item = as.numeric(gsub("PSY_BOKI_SMS_", "", item))
+  ) %>% select(SID, item, numericValue)
 
+# some need to be turned around
+idx = c(1, 2, 3, 4, 9, 12, 14, 17, 20, 21, 22, 23)
+df.sms[df.sms$item %in% idx,]$numericValue = abs(df.sms[df.sms$item %in% idx,]$numericValue - 1)
+
+df.sms = df.sms %>%
+  group_by(SID) %>%
+  summarise(
+    SMS_total = sum(numericValue, na.rm = T)
+  )
 
 # PSY_BOKI_AQ
-df.aq = df %>% filter(questionnaire == "PSY_BOKI_AQ") 
+df.aq = df %>% filter(questionnaire == "PSY_BOKI_AQ") %>%
+  mutate(
+    numericValue = as.numeric(case_when(
+      substr(value,1,3) == "(1)" ~ 0,
+      substr(value,1,3) == "(2)" ~ 0,
+      substr(value,1,3) == "(3)" ~ 1,
+      substr(value,1,3) == "(4)" ~ 1
+    )),
+    item = as.numeric(gsub("PSY_PIPS_AQ_Q", "", item))
+  ) %>% select(SID, item, numericValue)
 
+# some need to be turned around
+idx = c(2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46)
+df.aq[df.aq$item %in% idx,]$numericValue = abs(df.aq[df.aq$item %in% idx,]$numericValue - 1)
+
+df.aq = df.aq %>%
+  group_by(SID) %>%
+  summarise(
+    aq_total = sum(numericValue, na.rm = T)
+  )
 
 # merge all together
 ls.df = list(df.demo, df.cft, df.mwt, df.bdi, df.bsl, df.adc, df.aq, df.rap, df.sms, df.spf, df.tas)
-df.sub = ls.df %>% reduce(full_join, by = "SID") %>% 
-  mutate(
-    age = as.numeric(age)
-  )
+df.sub = ls.df %>% reduce(full_join, by = "SID") 
 
 # add iq scores
 mwt = read_delim("MWT-norms.csv", show_col_types = F, delim = ";")
@@ -205,4 +274,4 @@ for (i in 1:nrow(df.sub)) {
   }
 }
 
-write_csv(df.sub, file = "df_centraXX.csv")
+write_csv(df.sub, file = "BOKI_centraXX.csv")
