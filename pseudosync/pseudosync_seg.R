669a8390d61d2c19cc23b4fe6cbb1657169f0188
IreneSophia <10planki@gmail.com>
2023-07-07 16:04:12 +0200

    Finished function to calculate pseudosync on the basis of a list of MEA or fakeMEA objects.

--- pseudosync/pseudosync_seg.R
+++ pseudosync/pseudosync_seg.R
@@ -9,17 +9,74 @@
 # position". This shuffled time series is then paired with an unchanged time 
 # series Y. Pseudosynchrony is calculated using the CCF function from rMEA. 
 # Input: 
-#     * mea: a list of MEA objects, can also be created using the fakeMEA function
+#     * mea: a list of MEA or fakeMEA objects
 #     * sampRate: sampling rate per second
-#     * n: how many permutations to perform per MEA object
 #     * lagSec, winSec, incSec: length of lag, window and increment in seconds
+#     * n: how many permutations to perform per MEA object (default = 100)
+#     * peak: whether to apply peak picking or take grandaverage (default = F)
 #     * r2Z: whether to apply z transformation (BOOLEAN, default T)
 #     * ABS: whether to report absolute values (BOOLEAN, default T)
 # Output:
 #     * df.psync: data frame in long format containing pseudosync values
 #
 
-pseudosync_seg = function(mea, sampRate, n, lagSec, winSec, incSec, r2Z = T, ABS = T) {
+# Packages ----------------------------------------------------------------
+
+# if packman is not installed yet, install it
+if(!("pacman" %in% installed.packages()[,"Package"])) install.packages("pacman")
+pacman::p_load(tidyverse, rMEA) 
+
+# Function ----------------------------------------------------------------
+
+pseudosync_seg = function(mea, sampRate, lagSec, winSec, incSec, 
+                          n = 100, peak = F, r2Z = T, ABS = T) {
+  
+  # create new data frame for pseudosync values
+  cols = c("name", "psync")
+  df.psync = data.frame(matrix(nrow = 0, ncol = length(cols)))
+  colnames(df.psync) = cols
+  
+  # check if there are elements in the list
+  if (length(mea) < 1) stop('There are no elements in the list provided!')
+
+  # go through all list elements
+  for (i in 1:length(mea)) {
+    
+    # divide left or right into segments
+    mea_1 = split(mea[[i]][["MEA"]][[1]], floor(seq_along(mea[[i]][["MEA"]][[1]])/(winSec*sampRate)))
+    mea_2 = split(mea[[i]][["MEA"]][[2]], floor(seq_along(mea[[i]][["MEA"]][[2]])/(winSec*sampRate)))
+    
+    # shuffle each side n/2 times
+    for (j in 1:(n/2)) {
+      
+      # shuffling first person
+      mea[[i]][["MEA"]][,1] = unlist(sample(mea_1))
+      mea[[i]][["MEA"]][,2] = unlist(mea_2)
+      mea.ccf = MEAccf(mea[[i]], lagSec, winSec, incSec, r2Z, ABS)
+      # compute pseudosync
+      if (peak) {
+        psync = mean(apply(mea.ccf[["ccf"]], 1, max, na.rm = T ), na.rm = T)
+      } else {
+        psync = mea.ccf[["ccfRes"]][["grandAver"]]
+      }
+      # create new row in the data frame
+      df.psync[nrow(df.psync) + 1,] = c(names(mea)[i], psync)      
+      
+      # shuffling second person
+      mea[[i]][["MEA"]][,1] = unlist(mea_1)
+      mea[[i]][["MEA"]][,2] = unlist(sample(mea_2))
+      mea.ccf = MEAccf(mea[[i]], lagSec, winSec, incSec, r2Z, ABS)
+      # compute pseudosync
+      if (peak) {
+        psync = mean(apply(mea.ccf[["ccf"]], 1, max, na.rm = T ), na.rm = T)
+      } else {
+        psync = mea.ccf[["ccfRes"]][["grandAver"]]
+      }
+      # create new row in the data frame
+      df.psync[nrow(df.psync) + 1,] = c(names(mea)[i], psync)
+    }
+  }
   
   return(df.psync)
+  
 }
